09ad695f0da78d621b69b563545d1244b7831066
Mahmud Apak <apakmahmud@gmail.com>
2021-05-21 22:11:04 +0300

    Remove public path from webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,6 @@ module.exports = (env) => ({
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: env.dev ? '/' : './',
   },
   plugins: [
     new HtmlWebpackPlugin({
